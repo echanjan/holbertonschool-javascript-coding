529666a00d16677ea05ec6a4ba846d0a4655cd8e
echanjan <erickchanjan300696@gmail.com>
2024-04-07 02:04:00 -0500

    update

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require("request");
+const url = process.argv[2];
+
+if (!url) {
+  console.log("Debe proporcionar un URL");
+  process.exit(1);
+}
+
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
