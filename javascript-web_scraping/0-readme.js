50c72403b10d6342796cda672a34cff98e0d1bb0
echanjan <erickchanjan300696@gmail.com>
2024-04-06 23:04:35 -0500

    update

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error("Error: ruta no valida.");
+  console.error('Error: ruta no valida.');
   process.exit(1);
 }
 
-fs.readFile(filePath, "utf-8", (error, data) => {
+fs.readFile(filePath, 'utf-8', (error, data) => {
   if (error) {
     console.error(error);
   } else {
