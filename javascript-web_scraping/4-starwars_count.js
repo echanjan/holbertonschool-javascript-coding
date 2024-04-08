8d7079cfacefc83c3c51726b5a4f5b824c96cdb7
echanjan <erickchanjan300696@gmail.com>
2024-04-07 20:56:14 -0500

    update

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,35 +2,28 @@
 
 const request = require("request");
 
-// Obtener la URL de la API de la línea de comandos
 const apiUrl = process.argv[2];
 
-// Verificar si se proporcionó una URL
 if (!apiUrl) {
   console.error("Error: Debes proporcionar la URL de la API de Star Wars.");
   process.exit(1);
 }
 
-// ID del personaje "Wedge Antilles"
 const characterId = "18";
 
-// Realizar la solicitud GET a la API de Star Wars
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
     process.exit(1);
   }
 
-  // Convertir la respuesta a formato JSON
   const films = JSON.parse(body);
 
-  // Filtrar las películas donde el personaje "Wedge Antilles" está presente
   const moviesWithWedge = films.results.filter((film) =>
     film.characters.includes(
       `https://swapi-api.hbtn.io/api/people/${characterId}/`
     )
   );
 
-  // Imprimir el número de películas donde "Wedge Antilles" está presente
   console.log(moviesWithWedge.length);
 });
