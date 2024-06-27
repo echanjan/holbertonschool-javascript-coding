d24c791584c3f36fce6d806c534705b8f4a64ee5
echanjan <113923012+echanjan@users.noreply.github.com>
2024-06-26 21:48:13 -0500

    fix: lint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function Person(data) {
-  const [firstname, lastname, age, field] = data.split(",");
+  const [firstname, lastname, age, field] = data.split(',');
   this.firstname = ` ${firstname}`;
   this.lastname = lastname;
   this.age = age;
@@ -36,24 +36,24 @@ function getInfo(personObj, field, condition) {
 
 function stats(persons) {
   const personObj = getPersons(persons);
-  const cs = getInfo(personObj, "field", "CS");
-  const swe = getInfo(personObj, "field", "SWE");
+  const cs = getInfo(personObj, 'field', 'CS');
+  const swe = getInfo(personObj, 'field', 'SWE');
 
   console.log(`Number of students: ${personObj.length}`);
   console.log(
-    `Number of students in CS: ${cs.total}. List:${cs.names.join(",")}`
+    `Number of students in CS: ${cs.total}. List:${cs.names.join(',')}`,
   );
   console.log(
-    `Number of students in SWE: ${swe.total}. List:${swe.names.join(",")}`
+    `Number of students in SWE: ${swe.total}. List:${swe.names.join(',')}`,
   );
 }
 
 function countStudents(filePath) {
   if (!fs.existsSync(filePath)) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   } else {
-    const content = fs.readFileSync(filePath, "utf-8");
-    const persons = content.split("\n").filter((line) => line.trim() !== "");
+    const content = fs.readFileSync(filePath, 'utf-8');
+    const persons = content.split('\n').filter((line) => line.trim() !== '');
     stats(persons);
   }
 }
--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,11 +1,11 @@
 // Create a small HTTP server using Node's HTTP module
 
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
 class Person {
   constructor(data) {
-    const [firstname, lastname, age, field] = data.split(",");
+    const [firstname, lastname, age, field] = data.split(',');
     this.firstname = ` ${firstname}`;
     this.lastname = lastname;
     this.age = age;
@@ -37,13 +37,13 @@ function getInfo(personObj, field, condition) {
 
 function stats(persons) {
   const personObj = getPersons(persons);
-  const cs = getInfo(personObj, "field", "CS");
-  const swe = getInfo(personObj, "field", "SWE");
+  const cs = getInfo(personObj, 'field', 'CS');
+  const swe = getInfo(personObj, 'field', 'SWE');
 
   const a = `Number of students: ${personObj.length}`;
-  const b = `Number of students in CS: ${cs.total}. List:${cs.names.join(",")}`;
+  const b = `Number of students in CS: ${cs.total}. List:${cs.names.join(',')}`;
   const c = `Number of students in SWE: ${swe.total}. List:${swe.names.join(
-    ","
+    ',',
   )}`;
 
   return `${a}\n${b}\n${c}`;
@@ -51,13 +51,13 @@ function stats(persons) {
 
 function countStudents(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, "utf-8", (err, data) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       } else {
-        const persons = data.split("\n").filter((line) => line.trim() !== "");
+        const persons = data.split('\n').filter((line) => line.trim() !== '');
         if (persons.length === 0) {
-          reject(new Error("Cannot load the database"));
+          reject(new Error('Cannot load the database'));
         }
         resolve(stats(persons));
       }
@@ -68,13 +68,13 @@ function countStudents(filePath) {
 module.exports = countStudents;
 
 const handleRequest = (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader('Content-Type', 'text/plain');
 
-  if (req.url === "/") {
+  if (req.url === '/') {
     res.statusCode = 200;
-    res.end("Hello Holberton School!");
-  } else if (req.url === "/students") {
-    const database = process.argv[2] !== undefined ? process.argv[2] : "";
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const database = process.argv[2] !== undefined ? process.argv[2] : '';
     countStudents(database)
       .then((data) => {
         res.statusCode = 200;
@@ -86,7 +86,7 @@ const handleRequest = (req, res) => {
       });
   } else {
     res.statusCode = 400;
-    res.end("Not found");
+    res.end('Not found');
   }
 };
 
